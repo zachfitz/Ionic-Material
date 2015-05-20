162c6944c6a2dd222b343b1d61eb384df209b39a
Raimondo Butera <rai@rbutera.com>
2015-05-20 01:42:30 +0100

    quick first draft of an entry file

---
+++ src/js/ionic-material.js
@@ -0,0 +1,13 @@
+/**
+ * Ionic Material
+ * "Bundle Index" / "Entryfile"
+ * https://github.com/zachsoft/ionic-material
+ *
+ * Recommended: use a compiled version, especially in production!
+ */
+
+'use strict';
+
+// Import Dependencies
+require('./lib/_ink');
+require('./lib/_motion');
