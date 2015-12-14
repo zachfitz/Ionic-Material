87d55b41364ca6af7483eeb1cc471d2fee075178
Ascari <carlos.ascari.x@gmail.com>
2015-12-13 23:27:12 -0600

    Update _ink.js
    Exclude elements with the class `no-ink` from activating the ink effect. 
    
    So far using `ionicMaterialInk.displayEffect();` or not, are the only choices I am aware of.

--- src/js/lib/_ink.js
+++ src/js/lib/_ink.js
@@ -349,6 +349,7 @@ module.exports = function (angularApp) {
                 Effect.wrapInput($$(selectors));
 
                 Array.prototype.forEach.call($$(selectors), function(i) {
+                    if (i.className.indexOf('no-ink') !== -1) return;
                     if ('ontouchstart' in window) {
                         i.addEventListener('touchstart', Effect.show, false);
                         i.addEventListener('touchend', Effect.hide, false);
